b950a7751e6327a2484b9147cc75559dded8dfb9
codewithvishal78 <114843094+codewithvishal78@users.noreply.github.com>
2023-01-17 23:55:54 +0530

    %d print on the screen

---
+++ print %d.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char x='d';
+    printf("%%%c",x);
+    getch();
+}
+
+
+/*
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char x='%',y='d';
+    printf("%c%c",x,y);
+    getch();
+}
+*/
