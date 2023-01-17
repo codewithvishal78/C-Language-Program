8dce05e9fa764dfaee388e89319a927243b118ee
codewithvishal78 <114843094+codewithvishal78@users.noreply.github.com>
2023-01-18 00:19:17 +0530

    avg calculate and take input

---
+++ avg.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    float x,y,z,A;
+    printf("Enter three number \n");
+    scanf("%f%f%f",&x,&y,&z);
+    A=(x+y+z)/3;
+    printf("average=%f",A);
+    getch();
+}
