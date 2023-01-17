e8629a166df3649bc9d66e1b085d02bb695d1a02
codewithvishal78 <114843094+codewithvishal78@users.noreply.github.com>
2023-01-18 00:02:10 +0530

    take input and calculate area of circle

---
+++ area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float r,A;
+    printf("enter a circle radius\n");
+    scanf("%f",&r);
+    A=r*r*3.14;
+    printf("%f",A);
+    getch();
+}
